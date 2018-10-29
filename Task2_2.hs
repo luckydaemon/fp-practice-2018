5bc7b7c00fffa12305402a68c0bcac5fd799a69e
Leonid Bursikov <43574104+luckydaemon@users.noreply.github.com>
2018-10-29 23:38:25 +0300

    Update Task2_2.hs

--- Task2_2.hs
+++ Task2_2.hs
@@ -6,13 +6,17 @@ import Prelude hiding (foldl, foldr, unfoldr, map, concatMap,
     filter, maxBy, minBy, reverse, sum, product, elem)
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl = todo
+foldl x y []     = y                  
+foldl x y (z:zs) = foldl x (x y z) zs
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = todo
+foldr x y []     = y 
+foldr x y (z:zs) = x z (foldr x y zs) 
 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-unfoldr = todo
+unfoldr x y = case x y of
+    Just (z, y') -> z : unfoldr x y'
+    Nothing -> []
 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
@@ -24,37 +28,48 @@ reverse lst = foldl f [] lst where f t h = h:t
 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map = todo
+map f = foldr (\x xs -> f x : xs) []
 
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
-product = todo
+product = foldr (*) 1
 
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
-catMaybes = todo
+catMaybes = foldr (\x y -> case x of 
+                                Just x-> x:y
+                                Nothing -> y) []
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = todo
+diagonal mat = reverse $ snd $ foldl f (0, []) mat 
+    where f (l, result) lst = if length lst <= l then (l + 1, result) else (l + 1, lst !! l: result)
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot = todo
+filterNot con lst = foldr f [] lst
+    where f x res = if con x then res else (x: res)
 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
-elem = todo
+elem e lst = foldl(\res x -> if res then True else x == e)  False lst
 
 -- Список чисел в диапазоне [from, to) с шагом step
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
-rangeTo from to step = todo
+rangeTo from to step =  unfoldr (\from -> if from >= to then Nothing else Just (from, from+step)) from
+
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append = todo
+append x y = foldr (:) y x
 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = todo
+groups lst n = unfoldr (\x ->if null x then Nothing else Just $ split' n x) lst
+       where split' 0 y     = ([], y)
+             split' _ []    = ([], [])
+             split' n (y:ys)
+                | n < 0     = ([], (y:ys))
+                | otherwise = ((y:a), b)
+                 where (a, b) = split' (n - 1) ys
